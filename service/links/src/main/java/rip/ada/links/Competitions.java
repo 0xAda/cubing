1ca352b66acba8cb4b20d078e1bd9095e3e69c9f
Ada Cooke <me@ada.rip>
2025-10-17 23:01:59 +0100

    Point gun away from foot

--- service/links/src/main/java/rip/ada/links/Competitions.java
+++ service/links/src/main/java/rip/ada/links/Competitions.java
@@ -1,5 +1,7 @@
 package rip.ada.links;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rip.ada.wca.UnauthenticatedWcaApi;
 import rip.ada.wca.model.CompetitionInfo;
 import rip.ada.wcascraper.ScrapedCompetition;
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Competitions {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Competitions.class);
+
     private final WcaScraper wcaScraper;
     private final UnauthenticatedWcaApi wcaApi;
 
@@ -37,45 +41,60 @@ public class Competitions {
     }
 
     public void fetchCompetitions() {
+        final LocalDate now = LocalDate.now();
+
         final Map<String, Competition> updatedCompetitions = new HashMap<>();
         final Map<Sponsor, List<Competition>> updatedSponsoredCompetitions = new HashMap<>();
         final List<Competition> updatedLiveCompetitions = new ArrayList<>();
-        for (final CompetitionInfo competitionInfo : wcaApi.getUpcomingCompetitionsInCountry(CountryCode.GB, 5)) {
-            final rip.ada.wcif.Competition wcif = wcaApi.getCompetitionPublic(competitionInfo.id());
-            final ScrapedCompetition scrapedCompetition = wcaScraper.scrapeCompetition(competitionInfo.id());
 
-            final List<Sponsor> sponsors = new ArrayList<>();
-            for (final Sponsor sponsor : Sponsor.values()) {
-                for (final String tab : scrapedCompetition.tabs().keySet()) {
-                    final String lowerCaseTab = tab.toLowerCase(Locale.UK);
-                    if (lowerCaseTab.contains(sponsor.getName().toLowerCase(Locale.UK)) && !lowerCaseTab.contains("strathclyde")) {
-                        sponsors.add(sponsor);
-                    }
+        try {
+            final List<CompetitionInfo> listedCompetitions = wcaApi.getUpcomingCompetitionsInCountry(CountryCode.GB, 5);
+            for (final CompetitionInfo competitionInfo : listedCompetitions) {
+                final rip.ada.wcif.Competition wcif = wcaApi.getCompetitionPublic(competitionInfo.id());
+                final ScrapedCompetition scrapedCompetition = wcaScraper.scrapeCompetition(competitionInfo.id());
+
+                final boolean isIrishComp = scrapedCompetition.tabs().containsKey("Speedcubing Ireland");
+
+                final List<Sponsor> sponsors = getSponsors(scrapedCompetition);
+
+                final Schedule schedule = wcif.getSchedule();
+                final Competition competition = new Competition(wcif.getName(),
+                        wcif.getId(),
+                        isIrishComp,
+                        schedule.getStartDate(),
+                        schedule.getStartDate().plusDays(schedule.getNumberOfDays() - 1),
+                        wcif.getEvents().stream().map(Event::eventType).toList(),
+                        sponsors);
+
+                updatedCompetitions.put(wcif.getId(), competition);
+                for (final Sponsor sponsor : competition.sponsor()) {
+                    updatedSponsoredCompetitions.computeIfAbsent(sponsor, _ -> new ArrayList<>()).add(competition);
+                }
+
+                final LocalDate displayAfter = now.minusDays(5);
+                final LocalDate displayBefore = now.plusDays(2);
+                if (competition.startDate().isAfter(displayAfter) && competition.endDate().isBefore(displayBefore)) {
+                    updatedLiveCompetitions.add(competition);
                 }
             }
+            competitions = updatedCompetitions;
+            sponsoredCompetitions = updatedSponsoredCompetitions;
+            liveCompetitions = updatedLiveCompetitions;
+        } catch (final Exception e) {
+            LOGGER.error("Failed to fetch competitions", e);
+        }
+    }
 
-            final Schedule schedule = wcif.getSchedule();
-            final Competition competition = new Competition(wcif.getName(),
-                    wcif.getId(),
-                    scrapedCompetition.tabs().containsKey("Speedcubing Ireland"),
-                    schedule.getStartDate(),
-                    schedule.getStartDate().plusDays(schedule.getNumberOfDays() - 1),
-                    wcif.getEvents().stream().map(Event::eventType).toList(),
-                    sponsors);
-
-            updatedCompetitions.put(wcif.getId(), competition);
-            for (final Sponsor sponsor : competition.sponsor()) {
-                updatedSponsoredCompetitions.computeIfAbsent(sponsor, _ -> new ArrayList<>()).add(competition);
-            }
-
-            final LocalDate displayAfter = LocalDate.now().minusDays(5);
-            final LocalDate displayBefore = LocalDate.now().plusDays(2);
-            if (competition.startDate().isAfter(displayAfter) && competition.endDate().isBefore(displayBefore)) {
-                updatedLiveCompetitions.add(competition);
+    private static List<Sponsor> getSponsors(final ScrapedCompetition scrapedCompetition) {
+        final List<Sponsor> sponsors = new ArrayList<>();
+        for (final Sponsor sponsor : Sponsor.values()) {
+            for (final String tab : scrapedCompetition.tabs().keySet()) {
+                final String lowerCaseTab = tab.toLowerCase(Locale.UK);
+                if (lowerCaseTab.contains(sponsor.getName().toLowerCase(Locale.UK)) && !lowerCaseTab.contains("strathclyde")) {
+                    sponsors.add(sponsor);
+                }
             }
         }
-        competitions = updatedCompetitions;
-        sponsoredCompetitions = updatedSponsoredCompetitions;
-        liveCompetitions = updatedLiveCompetitions;
+        return sponsors;
     }
 }
