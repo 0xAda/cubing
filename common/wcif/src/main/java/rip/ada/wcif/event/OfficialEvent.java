574d2608e670cdfc1e7dbd05d3883e4a73972e74
Ada Cooke <me@ada.rip>
2025-12-12 12:55:07 +0000

    Add preferred round to wcif library

--- common/wcif/src/main/java/rip/ada/wcif/event/OfficialEvent.java
+++ common/wcif/src/main/java/rip/ada/wcif/event/OfficialEvent.java
@@ -2,39 +2,42 @@ package rip.ada.wcif.event;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 import rip.ada.wcif.EventType;
+import rip.ada.wcif.RoundFormat;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public enum OfficialEvent implements EventType {
-    THREE_BY_THREE("333", "3x3x3 Cube", false),
-    TWO_BY_TWO("222", "2x2x2 Cube", false),
-    FOUR_BY_FOUR("444", "4x4x4 Cube", false),
-    FIVE_BY_FIVE("555", "5x5x5 Cube", false),
-    SIX_BY_SIX("666", "6x6x6 Cube", false),
-    SEVEN_BY_SEVEN("777", "7x7x7 Cube", false),
-    THREE_BLIND("333bf", "3x3x3 Blindfolded", false),
-    FOUR_BLIND("444bf", "4x4x4 Blindfolded", false),
-    FIVE_BLIND("555bf", "5x5x5 Blindfolded", false),
-    ONE_HANDED("333oh", "3x3x3 One-Handed", false),
-    CLOCK("clock", "Clock", false),
-    MEGAMINX("minx", "Megaminx", false),
-    PYRAMINX("pyram", "Pyraminx", false),
-    SKEWB("skewb", "Skewb", false),
-    MULTI_BLIND("333mbf", "3x3x3 Multi-Blind", false),
-    SQUARE_ONE("sq1", "Square-1", false),
-    FMC("333fm", "3x3x3 Fewest Moves", false),
+    THREE_BY_THREE("333", "3x3x3 Cube", false, RoundFormat.AVERAGE_OF_FIVE),
+    TWO_BY_TWO("222", "2x2x2 Cube", false, RoundFormat.AVERAGE_OF_FIVE),
+    FOUR_BY_FOUR("444", "4x4x4 Cube", false, RoundFormat.AVERAGE_OF_FIVE),
+    FIVE_BY_FIVE("555", "5x5x5 Cube", false, RoundFormat.AVERAGE_OF_FIVE),
+    SIX_BY_SIX("666", "6x6x6 Cube", false, RoundFormat.MEAN_OF_THREE),
+    SEVEN_BY_SEVEN("777", "7x7x7 Cube", false, RoundFormat.MEAN_OF_THREE),
+    THREE_BLIND("333bf", "3x3x3 Blindfolded", false, RoundFormat.BEST_OF_THREE, THREE_BY_THREE),
+    FOUR_BLIND("444bf", "4x4x4 Blindfolded", false, RoundFormat.BEST_OF_THREE, FOUR_BY_FOUR),
+    FIVE_BLIND("555bf", "5x5x5 Blindfolded", false, RoundFormat.BEST_OF_THREE, FIVE_BY_FIVE),
+    ONE_HANDED("333oh", "3x3x3 One-Handed", false, RoundFormat.AVERAGE_OF_FIVE, THREE_BY_THREE),
+    CLOCK("clock", "Clock", false, RoundFormat.AVERAGE_OF_FIVE),
+    MEGAMINX("minx", "Megaminx", false, RoundFormat.AVERAGE_OF_FIVE),
+    PYRAMINX("pyram", "Pyraminx", false, RoundFormat.AVERAGE_OF_FIVE),
+    SKEWB("skewb", "Skewb", false, RoundFormat.AVERAGE_OF_FIVE),
+    MULTI_BLIND("333mbf", "3x3x3 Multi-Blind", false, RoundFormat.BEST_OF_THREE, THREE_BY_THREE),
+    SQUARE_ONE("sq1", "Square-1", false, RoundFormat.AVERAGE_OF_FIVE),
+    FMC("333fm", "3x3x3 Fewest Moves", false, RoundFormat.MEAN_OF_THREE, THREE_BY_THREE),
     //Removed events, still need to be able to parse in WCIF
-    MULTI_BLIND_OLD_STYLE("333mbo", "3x3x3 Multi-Blind Old Style", true),
-    THREE_BY_THREE_WITH_FEET("333ft", "3x3x3 With Feet", true),
-    MAGIC("magic", "Magic", true),
-    MASTER_MAGIC("mmagic", "Master Magic", true);
+    MULTI_BLIND_OLD_STYLE("333mbo", "3x3x3 Multi-Blind Old Style", true, RoundFormat.AVERAGE_OF_FIVE, THREE_BY_THREE),
+    THREE_BY_THREE_WITH_FEET("333ft", "3x3x3 With Feet", true, RoundFormat.AVERAGE_OF_FIVE, THREE_BY_THREE),
+    MAGIC("magic", "Magic", true, RoundFormat.AVERAGE_OF_FIVE),
+    MASTER_MAGIC("mmagic", "Master Magic", true, RoundFormat.AVERAGE_OF_FIVE);
 
     private static final Map<String, OfficialEvent> ID_TO_EVENT;
     private final String wcaEventId;
     private final String friendlyName;
     private final boolean removed;
+    private final RoundFormat preferredRoundFormat;
+    private final OfficialEvent baseEvent;
 
     static {
         final Map<String, OfficialEvent> map = new HashMap<>();
@@ -44,10 +47,20 @@ public enum OfficialEvent implements EventType {
         ID_TO_EVENT = Collections.unmodifiableMap(map);
     }
 
-    OfficialEvent(final String wcaEventId, final String friendlyName, final boolean removed) {
+    OfficialEvent(final String wcaEventId, final String friendlyName, final boolean removed, final RoundFormat preferredRoundFormat, final OfficialEvent baseEvent) {
         this.wcaEventId = wcaEventId;
         this.friendlyName = friendlyName;
         this.removed = removed;
+        this.preferredRoundFormat = preferredRoundFormat;
+        this.baseEvent = baseEvent;
+    }
+
+    OfficialEvent(final String wcaEventId, final String friendlyName, final boolean removed, final RoundFormat preferredRoundFormat) {
+        this.wcaEventId = wcaEventId;
+        this.friendlyName = friendlyName;
+        this.removed = removed;
+        this.preferredRoundFormat = preferredRoundFormat;
+        this.baseEvent = this;
     }
 
     @JsonValue
