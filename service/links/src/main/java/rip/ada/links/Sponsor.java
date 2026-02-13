9feb867025c243e444640ef0e881576cf6c4938c
Ada Cooke <me@ada.rip>
2026-02-13 22:06:49 +0000

    Add kewbz global

--- service/links/src/main/java/rip/ada/links/Sponsor.java
+++ service/links/src/main/java/rip/ada/links/Sponsor.java
@@ -3,6 +3,7 @@ package rip.ada.links;
 public enum Sponsor {
     KEWBZUK("KewbzUK", "https://kewbz.co.uk/", "https://static.ukca.org/KewbzUK_Logo.png"),
     KEWBZIE("KewbzIE", "https://kewbz.ie/", "https://static.ukca.org/KewbzIE_Logo.png"),
+    KEWBZGLOBAL("Kewbz.global", "https://kewbz.global/", "https://static.ukca.org/KewbzGlobal_Logo.png"),
     SPEEDCUBINGDOTORG("Speedcubing.org", "https://speedcubing.org/", "https://static.ukca.org/SpeedcubingDotOrg_Logo.png"),
     CUBOSS("Cuboss", "https://cuboss.com/", "https://static.ukca.org/Cuboss_Logo.png"),
     UTWISTCUBES("UTwistCubes", "https://www.utwistcubes.com/", "https://static.ukca.org/UTwistCubes_Logo.png"),
